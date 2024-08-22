ff36c29cfe3fca12f918469fde7fe81deadabb6a
cj <c.pizana.devv@gmail.com>
2024-08-21 18:14:38 -0700

    Messing with form css

--- react-vite/src/components/Recipes/RecipeFormPage.jsx
+++ react-vite/src/components/Recipes/RecipeFormPage.jsx
@@ -1,10 +1,13 @@
-import { useState,useRef } from "react";
+import { useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import * as recipeActions from '../../redux/recipe'
+import * as recipeActions from "../../redux/recipe";
+import { useNavigate } from "react-router-dom";
 
 function RecipeFormPage() {
   const currentUser = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [file, setFile] = useState(null);
   const [meal_name, setMealName] = useState("");
   const [course_type, setCourse] = useState("");
   const [prep_time, setPrepTime] = useState("");
@@ -12,8 +15,8 @@ function RecipeFormPage() {
   const [serving_size, setServingSize] = useState("");
   const [calories, setCalories] = useState("");
   const [isImageUploaded, setIsImageUploaded] = useState(false);
+  const [imageSrc, setImageSrc] = useState("");
   const imageInputRef = useRef(null);
-
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -35,54 +38,37 @@ function RecipeFormPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!file) {
-        alert("Please select a file first!");
-        return;
+      alert("Please select a file first!");
+      return;
     }
 
-    if (title.trim().length < 1) {
-        setTitleError("A title is required.");
-        return;
-    }
-
-    if (link && !isValidURL(link)) {
-        setLinkError("Please enter a valid URL.");
-        return;
-    }
     const newRecipe = {
-            user_id : currentUser,
-            meal_name,
-            prep_time,
-            cook_time,
-            serving_size,
-            calories,
-            file
-    }
-    const data = await dispatch(recipeActions.addRecipe(newRecipe))
+      user_id: currentUser,
+      meal_name,
+      prep_time,
+      cook_time,
+      serving_size,
+      calories,
+      file,
+    };
+    const data = await dispatch(recipeActions.addRecipe(newRecipe));
     if (data.img) {
-        navigate(`/pin/${data.id}`); //nav to ViewPin
+      navigate(`/pin/${data.id}`); //nav to ViewPin
     } else {
-        alert("Error uploading file");
+      alert("Error uploading file");
     }
-  }
-  const isValidURL = (string) => {
-      try {
-          new URL(string);
-          return true;
-      } catch (err) {
-          return false;
-      }
   };
   const handleReplaceImage = () => {
-      imageInputRef.current.click();
+    imageInputRef.current.click();
   };
 
   const handlePublish = () => {
-      handleSubmit(new Event('submit', { cancelable: true, bubbles: true }));
+    handleSubmit(new Event("submit", { cancelable: true, bubbles: true }));
   };
 
   const handleDragOver = (e) => {
-      e.preventDefault(); // Prevent default behavior
-      e.stopPropagation(); // Prevent event bubbling
+    e.preventDefault(); // Prevent default behavior
+    e.stopPropagation(); // Prevent event bubbling
   };
 
   const handleDrop = (e) => {
@@ -90,53 +76,84 @@ function RecipeFormPage() {
     e.stopPropagation(); // Prevent event bubbling
     const droppedFile = e.dataTransfer.files[0];
     handleFile(droppedFile);
-};
+  };
 
   return (
     <div className="create-recipe">
+         {isImageUploaded && (
+                    <button className='publish-button-header' onClick={handlePublish}>
+                        Publish
+                    </button>
+                )}
       <form>
-        <div
-          className="choose-file"
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-        >
-          <input
-            type="file"
-            onChange={handleFileChange}
-            ref={imageInputRef}
-            style={{ display: "none" }}
-          />
-          {!isImageUploaded && (
-            <button
-              type="button"
-              className="choose-file-button"
-              onClick={() => imageInputRef.current.click()}
-            >
-              <i className="fa-solid fa-arrow-up-from-bracket"></i>
-              Choose a file or drag and drop it here
-            </button>
-          )}
-          {isImageUploaded && (
-            <div className="uploaded-image-container">
-              <img src={imageSrc} alt="Uploaded" />
-              <span></span>
+        <div className="inputs">
+          <div
+            className="choose-file"
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            <input
+              type="file"
+              onChange={handleFileChange}
+              ref={imageInputRef}
+              style={{ display: "none" }}
+            />
+            {!isImageUploaded && (
               <button
                 type="button"
-                className="replace-file-button"
-                onClick={handleReplaceImage}
+                className="choose-file-button"
+                onClick={() => imageInputRef.current.click()}
               >
-                Replace Image
+                <i className="fa-solid fa-arrow-up-from-bracket"></i>
+                Choose a file or drag and drop it here
               </button>
-            </div>
-          )}
-        </div>
-        <div>
-            <input type="text" value={meal_name}/>
-            <input type="text" value={course_type}/>
-            <input type="text" value={prep_time}/>
-            <input type="text" value={cook_time}/>
-            <input type="text" value={serving_size}/>
-            <input type="text" value={calories} />
+            )}
+            {isImageUploaded && (
+              <div className="uploaded-image-container">
+                <img src={imageSrc} alt="Uploaded" />
+                <span></span>
+                <button
+                  type="button"
+                  className="replace-file-button"
+                  onClick={handleReplaceImage}
+                >
+                  Replace Image
+                </button>
+              </div>
+            )}
+          </div>
+          <div className="text-inputs">
+            <input
+              type="text"
+              value={meal_name}
+              onChange={(e) => setMealName(e.target.value)}
+            />
+            <input
+              type="text"
+              value={course_type}
+              onChange={(e) => setCourse(e.target.value)}
+            />
+            <input
+              type="text"
+              value={prep_time}
+              onChange={(e) => setPrepTime(e.target.value)}
+            />
+            <input
+              type="text"
+              value={cook_time}
+              onChange={(e) => setCookTime(e.target.value)}
+            />
+            <input
+              type="text"
+              value={serving_size}
+              onChange={(e) => setServingSize(e.target.value)}
+            />
+            <input
+              type="text"
+              value={calories}
+              onChange={(e) => setCalories(e.target.value)}
+            />
+          </div>
         </div>
       </form>
     </div>
