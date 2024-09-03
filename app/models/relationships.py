a6a9fc30224cc04ebbcb23e7c9a96ccf6693f036
cj <c.pizana.devv@gmail.com>
2024-09-03 13:24:33 -0700

    Added prefix for prod to relationship tables

--- app/models/relationships.py
+++ app/models/relationships.py
@@ -1,16 +1,17 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .tag import Tag
+from .ingredient import Ingredient
 
 recipe_tags = db.Table(
     "recipe_tags",
-    db.Column("tag_id", db.Integer, db.ForeignKey("tags.id"), primary_key=True),
-    db.Column("recipe_id", db.Integer, db.ForeignKey("recipes.id"), primary_key=True),
+    db.Column("tag_id", db.Integer, db.ForeignKey(add_prefix_for_prod("tags.id")), primary_key=True),
+    db.Column("recipe_id", db.Integer, db.ForeignKey(add_prefix_for_prod("recipes.id")), primary_key=True),
 )
 
 recipe_ingredients = db.Table(
     "recipe_ingredients",
-    db.Column("ingredient_id", db.Integer, db.ForeignKey("ingredients.id"), primary_key=True),
-    db.Column("recipe_id", db.Integer, db.ForeignKey("recipes.id"), primary_key=True),
+    db.Column("ingredient_id", db.Integer, db.ForeignKey(add_prefix_for_prod("ingredients.id")), primary_key=True),
+    db.Column("recipe_id", db.Integer, db.ForeignKey(add_prefix_for_prod("recipes.id")), primary_key=True),
     db.Column('quantity', db.String, nullable=False),
     db.Column('unit', db.String, nullable=False)
 )
