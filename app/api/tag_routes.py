4779cc629ec82a7867f790aa42ac89453eaabc05
cj <c.pizana.devv@gmail.com>
2024-09-06 20:15:23 -0700

    Finished backend tag routes

--- app/api/tag_routes.py
+++ app/api/tag_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
-from flask_login import login_required
-from app.models import Tag, db, recipe_tags
+from flask_login import login_required, current_user
+from app.models import Tag, db, recipe_tags, Recipe
 from app.forms import TagForm
 
 tag_routes = Blueprint('tags', __name__)
@@ -26,53 +26,44 @@ def add_tag():
             
             
             
-#Add pin + tag to joint table 
-@tag_routes.route('/<int:pin_id>/<int:tag_id>/add-pin-tag')
+#Add recipe + tag to joint table 
+@tag_routes.route('/<int:recipe_id>/<int:tag_id>/add-recipe-tag')
 @login_required
-def add_recipe_tag(pin_id,tag_id):
-    curr_recipe = recipe_tags.query.filter(recipe.id == recipe_id and tag_id == tag_id).one_or_none()
-    
-    if not curr_recipe or curr_recipe.user_id != current_user.id:
-        return {'error': 'recipe not found'}, 404
-    
-    curr_tag = Tag.query.filter(Tag.id == tag_id).one_or_none()
-    if not curr_tag:
-        return {'message': 'Tag not found'}, 404   
+def add_recipe_tag(recipe_id,tag_id):
     
     recipe_tag_exists = recipe_tags.query.filter(recipe_tags.recipe_id == recipe_id, recipe_tags.tag_id == tag_id).one_or_none()
     
     if recipe_tag_exists:
         return {'message': 'recipe-Tag relationship already exists'}, 400
     
+    
     new_recipe_tag = recipe_tags(
         recipe_id = recipe_id,
-        tag_id = curr_tag.tag_id
+        tag_id = tag_id
     )
     
     db.session.add(new_recipe_tag)
     db.session.commit()
     return {'message': 'recipe-Tag relationship added successfully'}, 201
 
-#Delete Pin Tag
-@pin_routes.route('/<int:pin_id>/<int:tag_id>', methods=['DELETE'])
+#Delete Tag
+@tag_routes.route('/<int:recipe_id>/<int:tag_id>', methods=['DELETE'])
 @login_required
-def delete_pin_tag(pin_id,tag_id):
+def delete_recipe_tag(recipe_id,tag_id):
     #Current Tag
-    #Pin tag relationship we want to delete
-    pin_tag_to_delete = pin_tags.query.filter(pin_tags.tag_id == tag_id, pin_tags.pin_id == pin_id).one_or_none()
+    #Recipe tag relationship we want to delete
+    recipe_tag_to_delete = recipe_tags.query.filter(recipe_tags.tag_id == tag_id, recipe_tags.recipe_id == recipe_id).one_or_none()
 
+    curr_recipe = Recipe.query.filter(Recipe.id == recipe_id).one_or_none()
     
-    curr_pin = Pin.query.filter(Pin.id == pin_id).one_or_none()
+    if not curr_recipe or curr_recipe.user_id != current_user.id:
+        return {'error': 'recipe not found or unauthorized'}, 404
     
-    
-    if not curr_pin or curr_pin.user_id != current_user.id:
-        return {'error': 'Pin not found or unauthorized'}, 404
-    
-    if not pin_tag_to_delete:
-        return {'error': 'Pin-Tag relationship not found'}, 404
+    if not recipe_tag_to_delete:
+        return {'error': 'recipe-Tag relationship not found'}, 404
     
     #Checking if tag is being used elsewhere
-    tag_usage = pin_tags.query.filter(pin_tags.tag_id == tag_id).count()
+    tag_usage = recipe_tags.query.filter(recipe_tags.tag_id == tag_id).count()
     if tag_usage == 1:
         curr_tag = Tag.query.filter(Tag.id == tag_id).one_or_none()
         if curr_tag:
@@ -81,6 +72,6 @@ def delete_pin_tag(pin_id,tag_id):
         else:
             return {'error': 'Tag not found'}, 404
 
-    db.session.delete(pin_tag_to_delete)
+    db.session.delete(recipe_tag_to_delete)
     db.session.commit()
-    return "Tag/Pin-Tag has been deleted"
+    return "Tag/Recipe-Tag has been deleted"
