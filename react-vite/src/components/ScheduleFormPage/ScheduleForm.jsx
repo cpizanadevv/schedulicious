7ba7aca3b0102b24ae94df5a35bb3a499aba3707
cj <c.pizana.devv@gmail.com>
2024-09-19 21:15:27 -0700

    Schedule page is styled, schedule form is styled and made into modal

--- react-vite/src/components/ScheduleFormPage/ScheduleForm.jsx
+++ react-vite/src/components/ScheduleFormPage/ScheduleForm.jsx
@@ -1,46 +1,88 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
+import { DateRange } from "react-date-range";
+import "./ScheduleForm.scss";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { addDays, differenceInCalendarDays } from "date-fns";
 
 // ! Make into a modal
 function ScheduleForm() {
-    const dispatch = useDispatch();
-    const [startDate, setStartDate] = useState("");
-    const [endDate, setEndDate] = useState("");
-    const [errors, setErrors] = useState({});
-    const { closeModal } = useModal();
-    const user = useSelector((state) => state.session.user)
-    
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+  const dispatch = useDispatch();
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [errors, setErrors] = useState({});
+  const { closeModal, setModalContent } = useModal();
+  const user = useSelector((state) => state.session.user);
 
-      newSchedule = {
-        user_id:user.id,
-        start_date:startDate,
-        end_date: endDate
-      }
-  
-      const serverResponse = await dispatch(newSchedule);
-  
-      if (serverResponse) {
-        setErrors(serverResponse);
-      } else {
-        closeModal();
-        setErrors({});
-      }
+  const [selectionRange, setSelectionRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  const handleSelect = (ranges) => {
+    const { startDate, endDate } = ranges.selection;
+    const differenceInDays = differenceInCalendarDays(endDate, startDate);
+
+    if (differenceInDays > 6) {
+      setErrors({ date: "You can only select a range of up to 7 days." });
+      return;
+    }
+
+    setSelectionRange({
+      ...ranges.selection,
+      endDate: addDays(startDate, differenceInDays),
+    });
+    setErrors({})
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    newSchedule = {
+      user_id: user.id,
+      start_date: startDate,
+      end_date: endDate,
     };
-    
-    // ! Before closing modal, Confirm user doesn't want to make more schedules
+
+    const serverResponse = await dispatch(newSchedule);
+
+    if (serverResponse) {
+      setErrors(serverResponse);
+    } else {
+      closeModal();
+      setErrors({});
+    }
+  };
+
+  // ! Before closing modal, Confirm user doesn't want to make more schedules
   return (
-    <div className="schedule-page">
-      <form onSubmit={handleSubmit}>
-        <h3>Select a start date for you week:</h3>
-        <input type="date" name="" id="" />
-        {errors.startDate && <p>{errors.startDate}</p>}
-        <h3>Select a end date for you week:</h3>
-        <input type="date" name="" id="" />
-        {errors.endDate && <p>{errors.endDate}</p>}
+    <div className="create-schedule">
+      <div className="site-name">
+        <h1 className="signlog">Scheduliscious</h1>
+      </div>
+      <h2 className="modal-title">Create a schedule!</h2>
+      <form onSubmit={handleSubmit} className="schedule-form">
+        <div className="schedule-form-div">
+            <label>Select days for the week:</label>
+            <div
+              className="calendar">
+                <DateRange
+              ranges={[selectionRange]}
+              onChange={handleSelect}
+            />
+            <p>Start Date: {selectionRange.startDate.toDateString()}</p>
+            <p>End Date: {selectionRange.endDate.toDateString()}</p>
+            {errors.date && <p className="errors">{errors.date}</p>}
+            </div>
+            
+        </div>
+        <div className="schedule-submit">
         <button type="submit">Create your Meal Week</button>
+
+        </div>
       </form>
     </div>
   );
--- react-vite/src/components/ScheduleFormPage/SchedulePage.jsx
+++ react-vite/src/components/ScheduleFormPage/SchedulePage.jsx
@@ -1,19 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ScheduleForm from "./ScheduleForm";
 import { useState } from "react";
-import OpenModalMenuItem from '../OpenModalButton'
-import './SchedulePage.scss'
+import OpenModalButton from '../OpenModalButton/OpenModalButton'
+import "./SchedulePage.scss";
 
 function SchedulePage() {
-  const user = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.session.user);
   const [day, setDay] = useState({});
   const [dayAmount, setDayAmount] = useState(7);
 
-
   // Get User's schedules, add them to select
 
-
-  const schedule = [{id:1}];
+  const schedule = [{ id: 1 }];
 
   const [selectedSchedule, setSelectedSchedule] = useState(schedule[0].id);
 
@@ -26,17 +25,9 @@ function SchedulePage() {
   // Determine how many days are in selected week, add them to days as individual
   const dayDivs = Array.from({ length: dayAmount }, (_, index) => (
     <div key={index} className="day-div">
-      <label className="day-labels" >Day {index + 1}</label>
-      
+      <label className="day-labels">Day {index + 1}</label>
     </div>
   ));
-
-  const closeMenu = () => setShowMenu(false);
-  const [showMenu, setShowMenu] = useState(false);
-  const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
-    setShowMenu(!showMenu);
-  };
 
   return (
     <div className="schedule-page">
@@ -44,71 +35,66 @@ function SchedulePage() {
         <img src="https://aa-aws-proj-bucket.s3.us-west-2.amazonaws.com/Designer+(6).png" />
       </div>
       <div className="schedule-top">
-        <div className="schedule-form">
-          <OpenModalMenuItem
-                itemText="Create Schedule"
-                onItemClick={closeMenu}
-                modalComponent={<ScheduleForm />}
-              />
+        <div className="schedule-form-modal">
+        
+          <OpenModalButton
+            buttonText="Create Schedule"
+            modalComponent={<ScheduleForm />}
+          />
         </div>
         <div className="schedule-select">
           <select>
-          {schedule.map(schedule => (
-          <option key={schedule.id} value={schedule.id}>
-            {schedule.label}
-          </option>
-        ))}
+            {schedule.map((schedule) => (
+              <option key={schedule.id} value={schedule.id}>
+                {schedule.label}
+              </option>
+            ))}
           </select>
         </div>
         <div className="schedule">
           <label className="schedule-days-title">Schedule</label>
-          <div className="days">
-            {dayDivs}
-            
-          </div>
+          <div className="days">{dayDivs}</div>
         </div>
 
         <div className="schedule-middle">
-            <div className="link-buttons">
-                <button className="schedule-button">Browse for more Recipes</button>
+          <div className="link-buttons">
+            <button className="schedule-button">Browse for more Recipes</button>
 
-                {/* Might move later */}
-                <button className="schedule-button">Grocery List</button>
-
-            </div>
+            {/* Might move later */}
+            <button className="schedule-button">Grocery List</button>
+          </div>
         </div>
       </div>
 
       <div className="schedule-bottom">
         <div className="fave-recipes">
-            <div className="filter-sorts">
-                <select className="filter"></select>
-                <select className="sort"></select>
-                <input type="search" name="" id="" />
-            </div>
-            <div className="recipes">
-
-            </div>
+          <div className="filter-sorts">
+            <select className="filter"></select>
+            <select className="sort"></select>
+            <input type="search" name="" id="" />
+          </div>
+          <div className="recipes"></div>
         </div>
         <div className="day-meals">
-            <div className="meal-sections">
-                <h2>Breakfast</h2>
-            </div>
-            <div className="meal-sections">
-                <h2>Lunch</h2>
-
-            </div>
-            <div className="meal-sections">
-                <h2>Dinner</h2>
-            </div>
-            <div className="meal-sections">
-                <h2>Snacks/Dessert</h2>
-            </div>
+          <div className="meal-sections">
+            <h2>Breakfast</h2>
+          </div>
+          <div className="meal-sections">
+            <h2>Lunch</h2>
+          </div>
+          <div className="meal-sections">
+            <h2>Dinner</h2>
+          </div>
+          <div className="meal-sections">
+            <h2>Snacks/Dessert</h2>
+          </div>
         </div>
       </div>
       <div className="submit-button">
-        <button className="schedule-button" type="submit"> Finalize Meal Week</button>
-
+        <button className="schedule-button" type="submit">
+          {" "}
+          Finalize Meal Week
+        </button>
       </div>
     </div>
   );
