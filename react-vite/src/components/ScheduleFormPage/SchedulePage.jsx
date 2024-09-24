2f3c00eff431b64c0b71897caa302cadaa678a15
cj <c.pizana.devv@gmail.com>
2024-09-23 21:22:58 -0700

    Added more functioinality to schedules

--- react-vite/src/components/ScheduleFormPage/SchedulePage.jsx
+++ react-vite/src/components/ScheduleFormPage/SchedulePage.jsx
@@ -13,9 +13,8 @@ function SchedulePage() {
   const schedules = useSelector((store) => store.schedule.schedules);
   const favorites = useSelector((store) => store.recipe.recipes);
 
-  console.log("faves", favorites);
+  // console.log("faves", favorites);
   const allFavs = Object.values(favorites);
-  console.log("all", allFavs);
   const allSchedules = Object.values(schedules).map((schedule) => ({
     ...schedule,
     formattedStartDate: new Date(schedule.start_date)
@@ -32,6 +31,7 @@ function SchedulePage() {
   const [startDate, setStartDate] = useState();
   const [dayAmount, setDayAmount] = useState(0);
   const [dayNames, setDayNames] = useState([]);
+  const [mealPlan, setMealPlan] = useState([]);
   const [selectedSchedule, setSelectedSchedule] = useState({});
 
   // Get User's schedules, add them to select
@@ -73,11 +73,29 @@ function SchedulePage() {
       setDayNames(dayNamesArray);
     }
   };
-  // console.log("CC",selectedId)
 
-  // Get User's Favorite recipes
+  const onDragStart = (e, recipe) => {
+    e.dataTransfer.setData("recipeId", recipe.id);
+  };
 
-  // Determine how many days are in selected week, add them to days as individual
+  const allowDrop = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const recipeId = e.dataTransfer.getData("recipeId");
+    if (recipeId) {
+      setMealPlan((prev) => [
+        ...prev,
+        {
+          schedule_id: selectedSchedule.id,
+          recipe_id: Number(recipeId),
+          day_of_week: selectedId,
+        },
+      ]);
+    }
+  };
 
   return (
     <div className="schedule-page">
@@ -87,11 +105,12 @@ function SchedulePage() {
       <div className="schedule-top">
         {schedules ? (
           <div className="schedule-select">
+            <h2>Choose a schedule</h2>
             <select
               onChange={handleScheduleChange}
               className="schedule-selector"
             >
-              <option value="">Choose a schedule</option>
+              <option value="">Choose</option>
               {allSchedules.map((schedule) => (
                 <option key={schedule.id} value={schedule.id}>
                   {schedule.formattedStartDate}
@@ -115,6 +134,7 @@ function SchedulePage() {
         {dayAmount > 0 && (
           <div className="schedule">
             <label className="schedule-days-title">Schedule</label>
+            <h3>Choose a day to add meals to:</h3>
             <div className="days">
               {dayNames.map((dayName, index) => (
                 <div
@@ -136,12 +156,6 @@ function SchedulePage() {
               <button className="schedule-button">
                 Browse for more Recipes
               </button>
-              <div className="submit-button">
-                <button className="schedule-button" type="submit">
-                  {" "}
-                  Finalize Meal Week
-                </button>
-              </div>
               <div className="schedule-form-modal">
                 <OpenModalButton
                   buttonText="Create Schedule"
@@ -164,9 +178,17 @@ function SchedulePage() {
                 {allFavs &&
                   allFavs.map((recipe) => (
                     <div className="schedule-recipe">
-                      <div className="schedule-recipe-img">
+                      <div
+                        className="schedule-recipe-img"
+                        draggable="true"
+                        onDragStart={(e) => onDragStart(e, recipe)}
+                      >
                         {recipe.img && (
-                          <img src={recipe.img} alt={recipe.meal_name} />
+                          <img
+                            src={recipe.img}
+                            alt={recipe.meal_name}
+                            className="schedule-recipe-img"
+                          />
                         )}
                         <div className="overlay">
                           <div className="overlay-text">{recipe.meal_name}</div>
@@ -179,17 +201,43 @@ function SchedulePage() {
             <div className="day-meals">
               <div className="meal-sections">
                 <h2>Breakfast</h2>
+                <div
+                  className="meals"
+                  onDragOver={allowDrop}
+                  onDrop={(e) => handleDrop(e, "Breakfast")}
+                ></div>
               </div>
               <div className="meal-sections">
                 <h2>Lunch</h2>
+                <div
+                  className="meals"
+                  onDragOver={allowDrop}
+                  onDrop={(e) => handleDrop(e, "Lunch")}
+                ></div>
               </div>
               <div className="meal-sections">
                 <h2>Dinner</h2>
+                <div
+                  className="meals"
+                  onDragOver={allowDrop}
+                  onDrop={(e) => handleDrop(e, "Dinner")}
+                ></div>
               </div>
               <div className="meal-sections">
                 <h2>Snacks/Dessert</h2>
+                <div
+                  className="meals"
+                  onDragOver={allowDrop}
+                  onDrop={(e) => handleDrop(e, "Snacks/Dessert")}
+                ></div>
               </div>
             </div>
+          </div>
+          <div className="submit-button">
+            <button className="schedule-button" type="submit">
+              {" "}
+              Finalize Meal Day
+            </button>
           </div>
         </div>
       )}
