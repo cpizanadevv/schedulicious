65fdd6622761818f089dad7e20a0c5203a073f9f
cj <c.pizana.devv@gmail.com>
2024-10-01 15:01:59 -0700

    adding more css

--- react-vite/src/components/RecipePage/RecipePage.jsx
+++ react-vite/src/components/RecipePage/RecipePage.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as recipeActions from "../../redux/recipe";
 import { useParams } from "react-router-dom";
+import "./RecipePage.scss";
 
 function RecipePage() {
   const dispatch = useDispatch();
@@ -11,7 +12,7 @@ function RecipePage() {
   const recipe = useSelector((state) => state.recipe.recipe);
   // console.log('state', recipe)
   const currRecipe = Object.values(recipe);
-  // console.log(currRecipe) 
+  // console.log(currRecipe)
 
   useEffect(() => {
     dispatch(recipeActions.getSingleRecipe(recipeId));
@@ -28,38 +29,38 @@ function RecipePage() {
                 <h2>{recipe.meal_name}</h2>
               </div>
             </div>
-            <div>
-              {/* <div>Allergens</div> */}
-            </div>
-            <div className="recipe-img">
-              <img src={recipe.img} alt={recipe.name} />
-            </div>
+            <div>{/* <div>Allergens</div> */}</div>
             <div className="nutri"></div>
-            <div>
+            <div className="recipe-pg-top">
+              <div className="recipe-pg-img">
+                <img src={recipe.img} alt={recipe.name} />
+              </div>
               <div className="equipment"></div>
               <div className="cookTime">
                 <p>Prep Time: {recipe.prep_time}</p>
                 <p>Cook Time: {recipe.cook_time}</p>
                 <p>Serving Size: {recipe.serving_size}</p>
-                </div>
+              </div>
               <div className="macros"></div>
             </div>
             <div>
-                <div>
-                    <ul>
-                        {recipe.ingredients && recipe.ingredients.map((ingredient) => (
-                            <li>{ingredient.name}</li>
-                        ))}
-                    </ul>
-                </div>
-                <div>
-                    <ul>
-                    {recipe.instructions && recipe.instructions.map((step) =>(
-                        <li>{step}</li>
+              <div className="recipe-pg-bottom">
+                <div className="recipe-pg-ingredients">
+                <ul className="ingredients">
+                  {recipe.ingredients &&
+                    recipe.ingredients.map((ingredient) => (
+                      <li>{ingredient.name}</li>
                     ))}
-
-                    </ul>
-                </div>
+                </ul>
+              </div>
+              <div>
+                <ul className="instructions">
+                  {recipe.instructions &&
+                    recipe.instructions.map((step,index) => <li>Step {index+1} {step}</li>)}
+                </ul>
+              </div>
+              </div>
+              
             </div>
           </div>
         ))}
