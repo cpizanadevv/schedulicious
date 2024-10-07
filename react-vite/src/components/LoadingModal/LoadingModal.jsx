b9a18171ed1c65d5f82c76255cd38bf1df318533
cj <c.pizana.devv@gmail.com>
2024-10-07 13:34:49 -0700

    finished recipe update

--- react-vite/src/components/LoadingModal/LoadingModal.jsx
+++ react-vite/src/components/LoadingModal/LoadingModal.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import './LoadingModal.scss'
 
+
 function LoadingModal() {
+    
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
     return(
         <div className="loading-modal">
             <h2>Loading</h2>
