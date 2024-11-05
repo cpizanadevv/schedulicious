76c95facd4786a162d0b68d5137dc0f6c1ac3a12
cj <c.pizana.devv@gmail.com>
2024-11-05 15:50:03 -0800

    Wrapped loading on recipe cards

--- react-vite/src/components/AllRecipesPage/AllRecipes.jsx
+++ react-vite/src/components/AllRecipesPage/AllRecipes.jsx
@@ -57,11 +57,9 @@ function AllRecipesPage() {
     }
   };
 
-  if (loading) return <p>Loading...</p>;
-
   return (
     <div>
-      <div className="banner" ref={pageChange}>
+      <div className="banner">
         <img
           src="https://aa-aws-proj-bucket.s3.us-west-2.amazonaws.com/recipeBanner.png"
           alt=""
@@ -71,64 +69,71 @@ function AllRecipesPage() {
         <SearchBar />
       </div> */}
       <div className="filtering"></div>
-      <div className="all-recipes">
-        {recipes.length > 0 ? (
-          recipes.map((recipe) => (
-            <div className="recipe-card" key={`recipe-${recipe.id}`}>
-              <div className="meal-name">
-                <h2>{recipe.meal_name}</h2>
-                {user && recipe && (
-                  <div
-                    className="fav"
-                    onClick={() => handleFav(recipe.id)}
-                    onMouseEnter={() => setHoveredRecipeId(recipe.id)}
-                    onMouseLeave={() => setHoveredRecipeId(null)}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="all-recipes" ref={pageChange}>
+          {recipes.length > 0 ? (
+            recipes.map((recipe) => (
+              <div className="recipe-card" key={`recipe-${recipe.id}`}>
+                <div className="meal-name">
+                  <h2>{recipe.meal_name}</h2>
+                  {user && recipe && (
+                    <div
+                      className="fav"
+                      onClick={() => handleFav(recipe.id)}
+                      onMouseEnter={() => setHoveredRecipeId(recipe.id)}
+                      onMouseLeave={() => setHoveredRecipeId(null)}
+                    >
+                      {hoveredRecipeId === recipe.id || recipe.favorited ? (
+                        <FaStar />
+                      ) : (
+                        <FaRegStar />
+                      )}
+                    </div>
+                  )}
+                </div>
+                <hr />
+                <div className="recipe-info">
+                  <div className="recipe-img">
+                    <img src={recipe.img} alt="" />
+                  </div>
+                  <div className="recipe-details">
+                    <div className="timings">
+                      Prep time: {recipe.prep_time} | Cook time:{" "}
+                      {recipe.cook_time} | Serves: {recipe.serving_size}
+                    </div>
+                    <div className="recipe-ingredients">
+                      <h3>Ingredients:</h3>
+                      <ul className="ingredient-list">
+                        {recipe.ingredients &&
+                          recipe.ingredients.map((ingredient, index) => (
+                            <li
+                              key={`ingredient-${index}`}
+                              className="recipe-ingredient"
+                            >
+                              {ingredient.ingredient_name}
+                            </li>
+                          ))}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="to-recipe-div">
+                  <NavLink
+                    to={`/recipes/${recipe.id}`}
+                    className="to-recipe-nav"
                   >
-                    {hoveredRecipeId === recipe.id || recipe.favorited ? (
-                      <FaStar />
-                    ) : (
-                      <FaRegStar />
-                    )}
-                  </div>
-                )}
-              </div>
-              <hr />
-              <div className="recipe-info">
-                <div className="recipe-img">
-                  <img src={recipe.img} alt="" />
-                </div>
-                <div className="recipe-details">
-                  <div className="timings">
-                    Prep time: {recipe.prep_time} | Cook time:{" "}
-                    {recipe.cook_time} | Serves: {recipe.serving_size}
-                  </div>
-                  <div className="recipe-ingredients">
-                    <h3>Ingredients:</h3>
-                    <ul className="ingredient-list">
-                      {recipe.ingredients &&
-                        recipe.ingredients.map((ingredient, index) => (
-                          <li
-                            key={`ingredient-${index}`}
-                            className="recipe-ingredient"
-                          >
-                            {ingredient.ingredient_name}
-                          </li>
-                        ))}
-                    </ul>
-                  </div>
+                    <button className="to-recipe">See full recipe</button>
+                  </NavLink>
                 </div>
               </div>
-              <div className="to-recipe-div">
-                <NavLink to={`/recipes/${recipe.id}`} className="to-recipe-nav">
-                  <button className="to-recipe">See full recipe</button>
-                </NavLink>
-              </div>
-            </div>
-          ))
-        ) : (
-          <h2>No Recipes</h2>
-        )}
-      </div>
+            ))
+          ) : (
+            <h2>No Recipes</h2>
+          )}
+        </div>
+      )}
       {recipes.length > 0 && (
         <div className="pagination">
           <button disabled={currPg === 1} onClick={handlePrevPage}>
