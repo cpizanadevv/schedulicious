27b6f37a64f7ecb3baa3c1b928b03fa8032895be
cj <c.pizana.devv@gmail.com>
2024-11-11 14:02:59 -0800

    adding edit mode to calendar view

--- react-vite/src/components/CalenderViewSchedule/Calendar.jsx
+++ react-vite/src/components/CalenderViewSchedule/Calendar.jsx
@@ -1,16 +1,28 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import './Calendar.scss'
+import "./Calendar.scss";
+
 
 function Calendar() {
   const calendarRef = useRef(null);
+  const [isEditMode, setIsEditMode] = useState(false);
 
   const handleViewChange = (view) => {
     const calendarApi = calendarRef.current.getApi();
     calendarApi.changeView(view);
+  };
+
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode);
+  };
+
+  const handleWeekHover = (weekStart, weekEnd) => {
+    if (isEditMode) {
+      
+    }
   };
 
   const handleDateClick = (info) => {
@@ -20,16 +32,6 @@ function Calendar() {
   const handleSelect = (info) => {
     // alert('Selected from: ' + info.startStr + ' to: ' + info.endStr);
   };
-
-  const handleDayCellClassNames = (arg) => {
-    const today = new Date();
-    const cellDate = new Date(arg.date);
-    
-    if (cellDate < today.setHours(0, 0, 0, 0)) {
-        return ['past-date'];
-    }
-    return [];
-};
 
   return (
     <div>
@@ -43,7 +45,11 @@ function Calendar() {
         <button onClick={() => handleViewChange("timeGridDay")}>
           Day View
         </button>
-        
+      </div>
+      <div>
+      <button onClick={toggleEditMode}>
+        {isEditMode ? "Exit Edit Mode" : "Enter Edit Mode"}
+      </button>
       </div>
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -51,8 +57,45 @@ function Calendar() {
         selectable={true}
         dateClick={handleDateClick}
         select={handleSelect}
-        dayCellClassNames={handleDayCellClassNames}
         ref={calendarRef}
+        views={{
+          timeGridWeek: {
+            slotLabelFormat: [],
+            slotMinTime: "24:00:00",
+            slotMaxTime: "24:00:00",
+          },
+          timeGridDay: {
+            slotLabelFormat: [],
+            slotMinTime: "24:00:00",
+            slotMaxTime: "24:00:00",
+          },
+        }}
+        dayCellContent={(arg) => {
+          const { date } = arg;
+          const startOfWeek = new Date(date);
+          startOfWeek.setDate(date.getDate() - date.getDay());
+          const endOfWeek = new Date(startOfWeek);
+          endOfWeek.setDate(startOfWeek.getDate() + 6);
+
+          return (
+            <div
+              onMouseEnter={() => handleWeekHover(startOfWeek, endOfWeek)}
+              className="day-cell-content"
+            >
+              {date.getDate()}
+              {isEditMode && (
+                <div className="week-actions">
+                  <button onClick={() => addRecipes(startOfWeek, endOfWeek)}>
+                    Add Recipes
+                  </button>
+                  <button onClick={() => clearRecipes(startOfWeek, endOfWeek)}>
+                    Clear Recipes
+                  </button>
+                </div>
+              )}
+            </div>
+          );
+        }}
       />
     </div>
   );
