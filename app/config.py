daa099a09bd2520a73139ca6fa75b8c8a9d71b1e
cj <c.pizana.devv@gmail.com>
2026-04-06 23:09:03 -0700

    Made changes to config file to include ssl

--- app/config.py
+++ app/config.py
@@ -1,3 +1,17 @@
+# import os
+
+
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+#     FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     # SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
+#     # (only 'postgresql') but heroku's postgres add-on automatically sets the
+#     # url in the hidden config vars to start with postgres.
+#     # so the connection uri must be updated here (for production)
+#     SQLALCHEMY_DATABASE_URI = os.environ.get(
+#         'DATABASE_URL').replace('postgres://', 'postgresql://')
+#     SQLALCHEMY_ECHO = True
 import os
 
 
@@ -5,10 +19,19 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
-    # (only 'postgresql') but heroku's postgres add-on automatically sets the
-    # url in the hidden config vars to start with postgres.
-    # so the connection uri must be updated here (for production)
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+
+    uri = os.environ.get('DATABASE_URL')
+
+    if uri and uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
+
+    # 🔥 ADD THIS
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "sslmode": "require"
+        }
+    }
+
     SQLALCHEMY_ECHO = True
